9e55cace97d3046cb02f97d4d49ec0b23343b46e
ajay32910 <a.ajay.hitk@gmail.com>
2023-10-25 11:59:17 +0530

    n

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//     Input: ["lint","code","love","you"]
+// Output: ["lint","code","love","you"]
+// Explanation:
+// One possible encode method is: "lint:;code:;love:;you"
+vector<string>input= {"lint","code","love","you"};
+//vector<string>input= {"lint"};
+string en="";
+for(auto s:input){
+    en=en+to_string(s.size())+s;
+    //en=en+"#";
+}
+}
+now some change
+//
